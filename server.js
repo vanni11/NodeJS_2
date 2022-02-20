8f46f7270586ad91499d4dfbc57d15d18dcdd5e7
yuntaehun <varick137@gmail.com>
2022-02-21 02:36:56 +0900

    회원인증기능 (로그인, 마이페이지)

--- server.js
+++ server.js
@@ -13,10 +13,13 @@ app.use('/public', express.static('public'));
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 
+// env파일 사용하기위한 dotenv 라이브러리
+require('dotenv').config();
+
 /* ------------------------------------------------------------DB 연결------------------------------------------------------------*/
 var db;
 MongoClient.connect(
-'mongodb+srv://taehun:alfm@cluster0.rbzvi.mongodb.net/todoapp?retryWrites=true&w=majority'
+process.env.DB_URL
 , {useUnifiedTopology: true} //warning메세지 표시 안되도록해줌
 , function(err, client) {
     if(err) return console.log('@@@' + err);
@@ -31,7 +34,7 @@ MongoClient.connect(
     // 서버 열기
     // 파라미터 : 서버띄울 포트번호, 띄운 후 실행할 코드
     // mongoDB설정한 후 여기로 옮김
-    app.listen(8080, function(){
+    app.listen(process.env.PORT, function(){
         console.log('listening on 8080');
     });
 })
@@ -137,3 +140,85 @@ app.put('/editpost', function(req, res){
         res.redirect('/list');
     });
 });
+
+
+/* ------------------------------------------------------------ 회원인증 기능 시작 ------------------------------------------------------------*/
+// 회원가입기능은 너무 복잡하므로 만들지않고 DB에 회원정보 직접생성함
+// 비밀번호 암호화도 여기서는 하지않음
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const res = require('express/lib/response');
+
+// 라이브러리 사용법이니 이해하는거 아니고 그냥 사용하기
+app.use(session({secret: '비밀코드', resave: true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', function(req, res){
+    res.render('login.ejs');
+});
+
+// passport.authenticate : 아이디,비번 검사하는 passport 문법
+// local : local 방식으로 로그인 요청
+app.post('/login', passport.authenticate('local', {
+    failureRedirect: '/fail' // 회원인증 실패시 /fail로 이동
+}) , function(req, res){
+    res.redirect('/mypage'); //회원인증 성공시 /로 이동
+});
+
+// [passport 세부설정]
+// Strategy : 인증하는 방법
+// 우리는 LocalStrategy를 사용할거고 아래에서 정의하고 있음 -> 깊게 이해는 안해도됨
+// 비번을 직접비교하는건 보안에 안좋으므로 나중에 암호화 추가해서 보완하기
+passport.use(new LocalStrategy({
+    // 설정 파라미터 4개
+    usernameField: 'id', // 사용자가 제출한 아이디가 어디 적혀있는지
+    passwordField: 'pw', // 사용자가 제출한 비밀번호가 어디 적혀있는지
+    session: true, // 세션으로 저장할것인지
+    passReqToCallback: false, // id,pw 외에 다른 정보 검증시 true로 설정하고 아래 function에 파라미터 추가 -> 알필요없음
+  }, function (inputID, inputPW, done) {
+    //console.log(inputID, inputPW);
+
+    // done(서버에러[DB연결 불가 등], 성공시 보내는 사용자 DB데이터, 에러메세지)
+    db.collection('login').findOne({ id: inputID }, function (err, result) {
+      if (err) return done(err);
+  
+      if (!result) return done(null, false, { message: '존재하지않는 아이디!' });
+      if (inputPW == result.pw) {
+        return done(null, result); // result가 serializeUser의 user로 전달됨!!
+      } else {
+        return done(null, false, { message: '비번 틀림' });
+      }
+    })
+  }));
+
+// 위에까지로 로그인 기능은 완성인데, 로그인 유지시키려면 아래처럼 세션만들기
+// id를 이용한 암호문을 세션으로 저장시키는 코드 (로그인 성공시 실행)
+passport.serializeUser(function(user, done){
+done(null, user.id); // 이후에는 세션의 id정보를 쿠키로 보내짐
+});
+
+// 해당 세션 데이터를 가지고있는게 DB에 있는지 검증 (마이페이지 접속시 실행)
+passport.deserializeUser(function(id, done){
+    // 가지고있는 세션으로 유저를 찾아서 유저정보를 넣어줌
+    db.collection('login').findOne({id: id/*user.id*/}, function(err, result){
+        done(null, result); // result가 req.user로 들어가짐(?)
+    });
+});
+
+/* ------------------------------------------------------------ 마이페이지 ------------------------------------------------------------*/
+app.get('/mypage', checkLogin/* 두번째 인자에 넣는 방법으로 미들웨어를 쓴다 */, function(req, res){
+    console.log(req.user);
+    res.render('mypage.ejs', {user: req.user});
+});
+
+// 미들웨어(함수) 생성
+function checkLogin(req, res, next){
+    if(req.user){ // 로그인 후 세션이 있으면 req.user는 존재함 (세션이 있는지만 확인됨)
+        next();
+    }else{
+        res.send('로그인이 필요합니다!');
+    }
+}
